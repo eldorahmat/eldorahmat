fc07c1a5e722c3ed0e9a2c16a2eca0746255b6bb
eldorahmat <eldorahmatsaputra@gmail.com>
2024-03-21 15:04:42 +0700

    first commit

---
+++ pertemuan 1/pertemuan1/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+   <h1> Hello World.</h1>
+
+    <buttonc />
+    <buttonc />
+    <textheader/>
+  </div>
+</template>
+
+<script setup>
+  import buttonc from './components/buttonc.vue'
+  import textheader from './components/textheader.vue'
+</script>
---
+++ pertemuan 1/pertemuan1/src/components/buttonc.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn">
+        Submit
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    background-color: aquamarine;
+    color: antiquewhite;
+    font-size: 28px;
+}
+</style>
